f93bc2fce58e15565778f0f5ef8684709c555ed5
German Monzon <german.monzon@wnco.com>
2020-04-27 22:29:11 -0300

    [graphql] Querying jobs and companies

--- GraphQL/job-board/client/src/requests.js
+++ GraphQL/job-board/client/src/requests.js
@@ -18,6 +18,24 @@ async function graphqlRequest(query, variables = {}) {
   return responseBody.data;
 }
 
+export async function loadCompany(id) {
+  const query = `query CompanyQuery($id: ID!) {
+    company(id: $id){
+      id
+      name
+      description
+      jobs {
+        id
+        title
+      }
+    }
+  }`;
+
+  const { company } = await graphqlRequest(query, {id});
+
+  return company;
+}
+
 export async function loadJob(id) {
   const query = `query JobQuery($id: ID!) {
     job(id: $id){
