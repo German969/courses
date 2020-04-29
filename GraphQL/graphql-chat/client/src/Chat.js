75fe4c5fbabb62ef07ea711048bdf1e233561188
German Monzon <german.monzon@wnco.com>
2020-04-28 22:35:24 -0300

    [graphql] Custom Chat Hook

--- GraphQL/graphql-chat/client/src/Chat.js
+++ GraphQL/graphql-chat/client/src/Chat.js
@@ -1,43 +1,17 @@
-import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
 import React from 'react';
-import { addMessageMutation, messageAddedSubscription, messagesQuery } from './graphql/queries';
+import { useChatMessages } from './hooks';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
 const Chat = ({user}) => {
-  // const [messages, setMessages] = useState([]);
-  // const {loading, error, data} = useQuery(messageQuery);
-  // useQuery(messagesQuery, {
-  //   onCompleted: ({messages}) => setMessages(messages)
-  // });
-  const { data } = useQuery(messagesQuery);
-  const messages = data ? data.messages : []
-
-  useSubscription(messageAddedSubscription, {
-    onSubscriptionData: ({client, subscriptionData}) => {
-      // setMessages(messages.concat(subscriptionData.data.messageAdded));
-      client.writeData({data: {
-        messages: messages.concat(subscriptionData.data.messageAdded)
-      }});
-    }
-  });
-
-  // const [addMessage, {loading, error, data, called}] = useMutation(addMessageMutation);
-  const [addMessage] = useMutation(addMessageMutation);
-
-  const handleSend = async (text) => {
-    await addMessage({variables: {input: {text}}});
-  };
-
-  //if (loading) console.log(queryLoading);
-  //if (error) console.log(queryError);
+  const { messages, addMessage } = useChatMessages();
 
   return (
     <section className="section">
       <div className="container">
         <h1 className="title">Chatting as {user}</h1>
         <MessageList user={user} messages={messages} />
-        <MessageInput onSend={handleSend} />
+        <MessageInput onSend={addMessage} />
       </div>
     </section>
   );
