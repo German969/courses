960326bfe70196d4564b8200f8f761e6a03e1fd9
German Monzon <gmonzon@makingsense.com>
2020-08-16 21:17:08 -0300

    [advanced-js-boot] vue bills app categories

---
+++ JavaScript/Advanced-JS-Bootcamp/VueJS/bill-tracker-app/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <main>
+    <AddCategory v-if="shouldShowAddCategory" v-on:addCategory="addCategory" />
+    <div v-else>
+      <NavBar
+        :categories="categories"
+        v-on:triggerShowAddCategory="triggerShowAddCategory"
+      />
+      <div class="container flex">
+        <div class="w-1/2">
+          <BillsTable />
+        </div>
+        <div class="w-1/2">
+          <Chart :bills="activeBills" />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+  import Vue from "vue"
+
+  import AddCategory from "./components/AddCategory.vue"
+  import AddBill from "./components/AddBill.vue"
+  import NavBar from "./components/NavBar.vue"
+  import Chart from "./components/Chart.vue"
+  import BillsTable from "./components/BillsTable.vue"
+
+  export default {
+    name: "app",
+    data() {
+      return {
+        bills: [],
+        categories: [],
+        shouldShowAddCategory: false
+      }
+    },
+    mounted() {
+      if (localStorage.getItem('categories')) {
+        this.categories = JSON.parse(localStorage.getItem('categories'))
+      }
+
+      if (!this.categories.length) {
+        this.shouldShowAddCategory = true
+      }
+    },
+    methods: {
+      addCategory(category) {
+        this.categories.push(category)
+        this.shouldShowAddCategory = false
+      },
+      triggerShowAddCategory() {
+        this.shouldShowAddCategory = true
+      }
+    },
+    watch: {
+      categories() {
+        localStorage.setItem('categories', JSON.stringify(this.categories))
+      }
+    },
+    components: {
+      AddCategory,
+      AddBill,
+      Chart,
+      BillsTable,
+      NavBar,
+    },
+  }
+</script>
