5ce50d81cf1645eab24d22e115ac9b97527e1e78
German Monzon <gmonzon@makingsense.com>
2020-08-18 15:25:36 -0300

    [advanced-js-boot] complete vue bill app

--- JavaScript/Advanced-JS-Bootcamp/VueJS/bill-tracker-app/src/App.vue
+++ JavaScript/Advanced-JS-Bootcamp/VueJS/bill-tracker-app/src/App.vue
@@ -2,16 +2,30 @@
   <main>
     <AddCategory v-if="shouldShowAddCategory" v-on:addCategory="addCategory" />
     <div v-else>
-      <NavBar
+      <AddBill
+        v-if="shouldShowAddBill"
         :categories="categories"
-        v-on:triggerShowAddCategory="triggerShowAddCategory"
+        v-on:addBill="addBill"
       />
-      <div class="container flex">
-        <div class="w-1/2">
-          <BillsTable />
-        </div>
-        <div class="w-1/2">
-          <Chart :bills="activeBills" />
+      <div v-else>
+        <NavBar
+          :categories="categories"
+          :activeCategory="activeCategory"
+          v-on:clearActiveCategory="clearActiveCategory"
+          v-on:setActiveCategory="setActiveCategory"
+          v-on:triggerShowAddCategory="triggerShowAddCategory"
+        />
+        <div class="container flex">
+          <div class="w-1/2 bg-gray-200">
+            <BillsTable
+              :bills="activeBills"
+              v-on:triggerShowAddBill="triggerShowAddBill"
+              v-on:removeBill="removeBill"
+            />
+          </div>
+          <div class="w-1/2 bg-gray-400 pt-4 pl-4 text-2xl">
+            <Chart :bills="activeBills" />
+          </div>
         </div>
       </div>
     </div>
@@ -20,6 +34,8 @@
 
 <script>
   import Vue from "vue"
+
+  Vue.use(require("vue-moment"))
 
   import AddCategory from "./components/AddCategory.vue"
   import AddBill from "./components/AddBill.vue"
@@ -33,7 +49,9 @@
       return {
         bills: [],
         categories: [],
-        shouldShowAddCategory: false
+        shouldShowAddCategory: false,
+        shouldShowAddBill: false,
+        activeCategory: ""
       }
     },
     mounted() {
@@ -41,7 +59,11 @@
         this.categories = JSON.parse(localStorage.getItem('categories'))
       }
 
-      if (!this.categories.length) {
+      if (localStorage.getItem('bills')) {
+        this.bills = JSON.parse(localStorage.getItem('bills'))
+      }
+
+      if (!this.bills.length && !this.categories.length) {
         this.shouldShowAddCategory = true
       }
     },
@@ -52,12 +74,42 @@
       },
       triggerShowAddCategory() {
         this.shouldShowAddCategory = true
+      },
+      triggerShowAddBill() {
+        this.shouldShowAddBill = true
+      },
+      addBill(bill) {
+        this.bills.push(bill)
+        this.shouldShowAddBill = false
+      },
+      removeBill(index) {
+        this.bills = this.bills
+          .slice(0, index)
+          .concat(this.bills.slice(index + 1, this.bills.length))
+      },
+      clearActiveCategory() {
+        this.activeCategory = ''
+      },
+      setActiveCategory(category) {
+        this.activeCategory = category
       }
     },
     watch: {
       categories() {
         localStorage.setItem('categories', JSON.stringify(this.categories))
-      }
+      },
+      bills() {
+        localStorage.setItem('bills', JSON.stringify(this.bills))
+      },
+    },
+    computed: {
+      activeBills() {
+        return this.bills
+          .filter((bill) =>
+            this.activeCategory ? bill.category === this.activeCategory : true
+          )
+          .sort((a, b) => (new Date(a.date) < new Date(b.date) ? 1 : -1))
+      },
     },
     components: {
       AddCategory,
