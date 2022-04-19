892cb0ceac6095966f8a090eceebb7ee9589ac03
German Monzon <german.monzon.95@gmail.com>
2022-04-18 23:12:09 -0300

    [micro-react-node] add tickets

---
+++ Microservices/Node-React/ticketing/tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,103 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { app } from '../../app';
+
+it('return a 404 if the provided id does not exists', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .set('Cookie', global.signin())
+    .send({
+      title: ' llkjgh',
+      price: 20
+    })
+    .expect(404)
+});
+
+it('return a 401 if the user is not authenticated', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .send({
+      title: ' llkjgh',
+      price: 20
+    })
+    .expect(401)
+});
+
+it('return a 401 if the user does not own the ticket', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'iuytew',
+      price: 20
+    })
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', global.signin())
+    .send({
+      title: 'vferea',
+      price: 25
+    })
+    .expect(401)
+});
+
+it('return a 400 if the user provides an invalid title or price', async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'iuytew',
+      price: 20
+    })
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: '',
+      price: 20
+    })
+    .expect(400)
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'opuiuiy',
+      price: -20
+    })
+    .expect(400)
+});
+
+it('update the ticket providing the correct values', async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'iuytew',
+      price: 20
+    })
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'new title',
+      price: 100
+    })
+    .expect(200)
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual('new title');
+  expect(ticketResponse.body.price).toEqual(100);
+});
