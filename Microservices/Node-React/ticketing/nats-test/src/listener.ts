96e15bf434c6148e8a20bafae56c21f43a53fc6e
German Monzon <german.monzon.95@gmail.com>
2022-04-20 22:15:20 -0300

    [micro-react-node] add nats test

---
+++ Microservices/Node-React/ticketing/nats-test/src/listener.ts
@@ -0,0 +1,42 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222'
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to nats');
+
+  stan.on('close', () => {
+    console.log('NATS connection closed');
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable() // sends all events from the past when subscribe only first time
+    .setDurableName('accounting-service'); // store successful events from the subscriptor and not processed and not deliver everything when restart
+  const subscription = stan.subscribe(
+    'ticket:created',
+    // 'orders-service-queue-group',
+    'queue-group-name',// Adding this to persist durable name subscription
+    options
+  );
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
+  });
+});
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
